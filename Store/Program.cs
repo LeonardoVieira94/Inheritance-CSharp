4a78f9a2c16360ad35db9d915ce4f13738912bcb
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-07-26 07:53:07 -0300

    Exercício teste de herança e polimorfismo

---
+++ Store/Program.cs
@@ -0,0 +1,59 @@
+﻿using Store.Entities;
+using System.Globalization;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the quantity of products: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Product> products = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Commom, used or imported(c/u/i)? ");
+                char opt = char.Parse(Console.ReadLine());
+                if (opt == 'c')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    products.Add(new Product(name, price));
+                }
+                else if (opt == 'i')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Customs Fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Manufacture Date(DD/MM/YYYY): ");
+                    DateTime mDate = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, mDate));
+                }
+
+            }
+            Console.WriteLine("PRICE TAGS: ");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
